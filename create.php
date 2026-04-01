e10c63b79939e1705fb51d143223779a3f5ca992
zoiadesarrollo-beep <zoia.desarrollo@gmail.com>
2026-04-01 08:54:40 +0200

    Add files via upload

--- create.php
+++ create.php
@@ -1,25 +1,5 @@
 <?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-function Conectarse() 
-{
-   if (!($link=mysqli_connect("tb-be04-linweb087.srv.teamblue-ops.net:3306","solofa_javiercm","kojikabuto2014"))) 
-   { 
-      echo "Error conectando a la base de datos."; 
-	  exit(); 
-   } 
-   if (!mysqli_select_db($link,"solofa_solofaltastu2014")) 
-   { 
-      echo "Error seleccionando la base de datos."; 
-      exit(); 
-   } 
-   return $link; 
-} 
-
-$link=Conectarse(); 
-mysqli_query ($link,"SET NAMES 'utf8'");
-
+include("funciones.php");
 
 $f = fopen("compra.csv", "r");
 
--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php
-session_start();
-function Conectarse() 
-{
-   if (!($link=mysqli_connect("zoia-n8n_gestion_cesta","my_user_basket","uzVz7VXy2W-4GK*"))) 
-   { 
-      echo "Error conectando a la base de datos."; 
-	  exit(); 
-   } 
-   if (!mysqli_select_db($link,"zoia-n8n")) 
-   { 
-      echo "Error seleccionando la base de datos."; 
-      exit(); 
-   } 
-   return $link; 
-} 
-
-$link=Conectarse(); 
-mysqli_query ($link,"SET NAMES 'utf8'");
+include("funciones.php");
 
 ?>
 <html lang="es">
